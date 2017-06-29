a14745ec1f6588987646e4edba3e59f5945a31c0
opasiak <jonatan@colburn.se>
2017-06-29 13:25:06 +0200

    Initial commit

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('ColburnOrtelius.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'ColburnOrtelius.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('ColburnOrtelius.model.Personnel', {
+    extend: 'ColburnOrtelius.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
